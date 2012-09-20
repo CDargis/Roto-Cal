eee6a42aa2dcb0462c3d5dd97c79526d837a8e90
CDargis <cdargi2@uic.edu>
2012-09-20 00:08:34 -0500

    adding git ignore file, resizing base, adding class

---
+++ calendarwidget.h
@@ -0,0 +1,18 @@
+#ifndef CALENDARWIDGET_H
+#define CALENDARWIDGET_H
+
+#include <QLabel>
+
+class CalendarWidget : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit CalendarWidget(QWidget *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // CALENDARWIDGET_H
