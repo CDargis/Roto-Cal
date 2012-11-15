170173b55306dac10bf50bd1dca943b4839d7750
gerron <gthurm2@uic.edu>
2012-11-14 18:20:40 -0600

    added a bunch of events and added additional functions to create_time_t

--- create_time_t.cpp
+++ create_time_t.cpp
@@ -24,6 +24,29 @@ int Create_time_t::militaryHour(Time1 time)
     return hour;
 }
 
+int Create_time_t::militaryHour(int hour, bool am)
+{
+
+    if(am){
+        if(hour == 12)
+        {
+            return 0;
+        }
+        else
+        {
+            return hour;
+        }
+    }
+    else
+    {
+        if(hour != 12)
+        {
+            return hour+12;
+        }
+    }
+    return hour;
+}
+
 time_t Create_time_t::createTime(Date date, Time1 time)
 {
         struct tm t;
@@ -33,6 +56,21 @@ time_t Create_time_t::createTime(Date date, Time1 time)
         t.tm_mday = date.getDay();
         t.tm_hour = militaryHour(time);
         t.tm_min = time.getMinute();
+        t.tm_sec = 0;
+        t.tm_isdst = -1;
+        raw = mktime(&t);
+        return raw;
+}
+
+time_t Create_time_t::createTime(int day, int month, int year, int minute, int hour, bool am)
+{
+        struct tm t;
+        time_t raw;
+        t.tm_year = year-1900;
+        t.tm_mon = month-1;
+        t.tm_mday = day;
+        t.tm_hour = militaryHour(hour, am);
+        t.tm_min = minute;
         t.tm_sec = 0;
         t.tm_isdst = -1;
         raw = mktime(&t);
--- create_time_t.h
+++ create_time_t.h
@@ -16,6 +16,15 @@ public:
    */
     static int militaryHour(Time1 time);
 
+    //! Military Hour used for time_t (static).
+   /*!
+   \param int as hour.
+   \param bool as am
+   \return Military hour converted from Time1
+   \sa createTime(Date date, Time1 time)
+   */
+    static int militaryHour(int hour, bool am);
+
     //! Create time_t for Event (static).
    /*!
    \param Date argument
@@ -24,6 +33,17 @@ public:
    \sa militaryHour(Time1 time)
    */
     static time_t createTime(Date date, Time1 time);
+
+    //! Create time_t for Event (static).
+   /*!
+   \param int day
+   \param int month
+   \param int minute
+   \param int hour
+   \return time_t created from date and time
+   \sa militaryHour(Time1 time)
+   */
+    static time_t createTime(int day, int month, int year, int minute, int hour, bool am);
 };
 
 #endif // CREATE_TIME_T_H
